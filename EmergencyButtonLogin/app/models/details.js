33f16f15f45253ef926116ab4936ad39d0647fc6
Adhikansh Mittal <adhikanshmittal@gmail.com>
2019-02-22 22:25:18 +0530

    login and signup

---
+++ EmergencyButtonLogin/app/models/details.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var DetailSchema = new Schema({
+    PatientPassword: String,
+});
+
+module.exports = mongoose.model('Details', DetailSchema);
---
+++ EmergencyButtonLogin/index.js
@@ -0,0 +1,73 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var Details = require("./app/models/details");
+var MongoClient = require("mongodb").MongoClient;
+var AES = require("crypto-js/aes");
+var SHA256 = require("crypto-js/sha256");
+
+// Configure app for bodyParser()
+// lets us grab data from the body of POST
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+app.use(bodyParser.json());
+
+// Set up port for server to listen on
+var port = process.env.PORT || 3002;
+
+// Connect to DB
+// var url = "mongodb://adhi:password123@ds141796.mlab.com:41796/signup";
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, (err, db) => {
+    const dbo = db.db("devsoc");
+
+    //API Routes
+    var router = express.Router();
+
+    // Routes will all be prefixed with /API
+    app.use("/api", router);
+
+    //MIDDLE WARE-
+    router.use(function (req, res, next) {
+        console.log("FYI...There is some processing currently going down");
+        next();
+    });
+
+    // test route
+    router.get("/", function (req, res) {
+        res.json({
+            message: "Welcome !"
+        });
+    });
+
+    router.route("/patientlogin").post(function (req, res) {
+        var person = new Details();
+        person.PatientPassword = req.body.PatientPassword;
+        dbo.collection("details").find({}).toArray(function (err, result) {
+            if (err) throw err;
+            else {
+                console.log(result);
+                if (result.PatientPassword[i] == person.PatientPassword) {
+                    console.log(`User is Authenticated Congo!`);
+                    return true;
+                }
+            }
+            console.log(`User is Not Authenticated`);
+            return false;
+        });
+    });
+
+
+});
+
+
+// Fire up server
+app.listen(port);
+
+// print friendly message to console
+console.log("Server listening on port " + port);
