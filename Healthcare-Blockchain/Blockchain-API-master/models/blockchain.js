c08ec84fece3288a8fdc7a063fe76261ebbc5b90
HrithikMittal <adhikanshmittal@gmail.com>
2019-02-24 00:05:32 +0530

    add fb plugin

---
+++ Healthcare-Blockchain/Blockchain-API-master/models/blockchain.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const Promise = require("bluebird");
+const Block = require("./block");
+
+const blockChainSchema = mongoose.Schema({
+  chain: [{
+    type: mongoose.Schema.ObjectId,
+    ref: "Block"
+  }]
+});
+
+/**
+ * @function addBlock
+ * @param {Object} data
+ */
+blockChainSchema.methods.addBlock = function (data, variable) {
+  console.log(variable);
+  return this.model("Blockchain")
+    .findOne()
+    .populate("chain", "hash")
+    .exec()
+    .then(chain => {
+      const index = this.chain.length;
+      const previousHash = index !== 0 ? chain.chain[index - 1].hash : "0";
+      let block = new Block({
+        index: index,
+        Content: variable,
+        previousHash: previousHash,
+        data: data
+        // Content: Content.Content
+      });
+      block.hash = block.calculateHash();
+      return block.save().then(savedBlock => {
+        this.chain.push(savedBlock._id);
+        return Promise.resolve(this);
+      });
+    })
+    .catch(err => Promise.reject(err));
+};
+
+/**
+ * @function validateChain
+ */
+
+blockChainSchema.methods.validateChain = function () {
+  return this.model("Blockchain")
+    .findOne()
+    .populate("chain")
+    .exec()
+    .then(chain => {
+      return Promise.map(chain.chain, (block, index) => {
+        let outcome = "Valid";
+        if (
+          index > 0 &&
+          chain.chain[index].previousHash !== chain.chain[index - 1].hash
+        )
+          outcome = "Invalid cause previous hash mismatch";
+        return outcome;
+      }).then(validities => {
+        return Promise.resolve(validities);
+      });
+    })
+    .catch(err => Promise.reject(err));
+};
+
+module.exports = mongoose.model("Blockchain", blockChainSchema);
