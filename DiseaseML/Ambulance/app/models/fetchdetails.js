1600e02c9b6e89c3d040d83c39229958903b7a8d
HrithikMittal <adhikanshmittal@gmail.com>
2019-02-24 11:45:40 +0530

    Public chat add

--- DiseaseML/Ambulance/app/models/fetchdetails.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var DetailSchema = new Schema({
-    PatientNo: Number,
-    PatientPassword: String,
-});
-
-module.exports = mongoose.model('Details', DetailSchema);
