8f14b3b5e18aa7aa4f28e424ce819bb5240864bd
HrithikMittal <adhikanshmittal@gmail.com>
2019-02-24 14:00:32 +0530

    complete

--- Chatapp/server.js
+++ Chatapp/server.js
@@ -1,52 +1,53 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
 var users = [];
 var connections = [];
 
-server.listen(process.env.PORT || 3005)
-console.log('server running....')
+port = "http://fe80::dccf:2bd2:9ace:48f6%19";
+server.listen(process.env.PORT || port);
+console.log("server running....");
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/main.html')
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/main.html");
 });
 
-app.get('/privatechat', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get("/privatechat", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
 //open connection with socketio
 
-io.sockets.on('connection', function (socket) {
+io.sockets.on("connection", function(socket) {
   connections.push(socket);
-  console.log('connected: %s sockets connected', connections.length)
+  console.log("connected: %s sockets connected", connections.length);
   //disconnect
-  socket.on('disconnect', function (data) {
+  socket.on("disconnect", function(data) {
     users.splice(users.indexOf(socket.username), 1);
     updateUsernames();
     connections.splice(connections.indexOf(socket), 1);
-    console.log("Disconnected: %s sockets connected", connections.length)
-  })
+    console.log("Disconnected: %s sockets connected", connections.length);
+  });
 
-  socket.on('new user', function (data, callback) {
+  socket.on("new user", function(data, callback) {
     callback(true);
     socket.username = data;
     users.push(socket.username);
-    updateUsernames()
+    updateUsernames();
   });
 
   function updateUsernames() {
-    io.sockets.emit('get users', users);
+    io.sockets.emit("get users", users);
   }
 
   //send message
   //listening for message
-  socket.on('send message', function (data) {
-    console.log(data)
-    io.sockets.emit('new message', {
+  socket.on("send message", function(data) {
+    console.log(data);
+    io.sockets.emit("new message", {
       msg: data,
       user: socket.username
     });
-  })
-})
+  });
+});
